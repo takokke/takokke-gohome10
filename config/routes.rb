18cc9fa51d7cf3000300939cbbab5f1a8582bf0f
takokke <takumicr1526@gmail.com>
2023-08-13 00:03:47 +0900

    サーバサイドを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'answers#index'
+  resources :answers
+  get '/input', to: 'answers#input'
+  get '/output', to: 'answers#output', as: :output
+  get '/correct/:input', to: 'answers#correct', as: :correct
+  get '/incorrect/:input', to: 'answers#incorrect', as: :incorrect
 end
